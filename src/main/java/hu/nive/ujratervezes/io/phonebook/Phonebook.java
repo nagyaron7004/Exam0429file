d756f0aa8de408b8fd9ae4cce5345c4555994cb6
nagyaron7004 <nagyaron7004@gmail.com>
2021-04-29 12:24:52 +0200

    finished

--- src/main/java/hu/nive/ujratervezes/io/phonebook/Phonebook.java
+++ src/main/java/hu/nive/ujratervezes/io/phonebook/Phonebook.java
@@ -1,3 +1,24 @@
 package hu.nive.ujratervezes.io.phonebook;
 
-public class Phonebook {}
+import java.io.File;
+import java.io.FileWriter;
+import java.util.Map;
+import java.io.IOException;
+
+public class Phonebook {
+    void exportPhonebook(Map<String, String> contacts, String output) {
+        if(contacts == null || output == null) {
+            throw new IllegalArgumentException();
+        }
+        File outputFile = new File(output);
+
+        try (FileWriter fileWriter = new FileWriter(outputFile,true)) {
+            for (Map.Entry<String,String> entry : contacts.entrySet()) {
+                fileWriter.write(entry.getKey()+": "+entry.getValue() + System.lineSeparator());
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
